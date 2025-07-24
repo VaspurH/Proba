a4af7c620f737499342d783b12eaf578bdb04381
VaspurH <hvaspur@gmail.com>
2025-07-24 12:07:10 +0300

    Добавлена функция установки времени

--- PythonProject1/reminde(напоминать).py
+++ PythonProject1/reminde(напоминать).py
@@ -5,6 +5,25 @@ import  datetime
 import time
 import pygame
 
+
+def set():
+    rem = sd.askstring("Время напоминания", " введите время напминания в формате ЧЧ:ММ (в 24 часовом формате)")
+    if rem:          #тикушее врмя
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка {e}")
+
+
+
+
 window = Tk()
 window.title("Напоминание")
 label = Label(text="Установите напоминание")
