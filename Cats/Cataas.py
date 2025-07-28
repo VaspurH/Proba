29f3f8666b6f698f0b156245081f39e14161e77b
VaspurH <hvaspur@gmail.com>
2025-07-28 16:22:53 +0300

    Создано окно и виджеты

--- Cats/Cataas.py
+++ Cats/Cataas.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = TK()
+window.title("Cats!")
+window.geometr("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.img = img
+
+window.mainloop()
