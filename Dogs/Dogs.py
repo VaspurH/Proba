0a230e699bcbb407fa62fb9d725d59836b06a2eb
VaspurH <hvaspur@gmail.com>
2025-07-31 08:32:59 +0300

    Создана функция загрузки ссылки на изображения

--- Dogs/Dogs.py
+++ Dogs/Dogs.py
@@ -1,27 +1,36 @@
 from tkinter import  *
-from tkinter import  meassagebox  as mt
+from tkinter import  messagebox  as mb
 import requestd
 from PIL import Image, ImegaTk
 from io import BytesIO
-from pyexpat.errors import messages
 
 
-def
+
+def get_dog_image():
+    try:
+        response = requestd.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data = response.json()
+        return data('message')
+    except Exception as e:
+        mb.showerror("Ошибка", f"Возникла ошибка при запросе к API{e}")
+        return None
 
 
 def show_image():
     image_url = get_dog_image()
     if image_url:
         try:
-            response = requestd.get(image_url, stream+True)
+            response = requestd.get(image_url, stream=True)
             response.raise_for_status()
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail(300, 300)
+            img = ImegaTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
-            mb.showerror("Ошибка", f"Возникла ошибка {e}")
+            mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображени {e}")
 
 window = Tk()
 window.title("Картинки с собачками")
