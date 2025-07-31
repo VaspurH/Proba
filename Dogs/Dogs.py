24ffb45e3f1aebc2525dcf325ec39fe15e2ccc17
VaspurH <hvaspur@gmail.com>
2025-07-31 10:37:00 +0300

    Добавлен прогресс-бар

--- Dogs/Dogs.py
+++ Dogs/Dogs.py
@@ -1,17 +1,18 @@
 from tkinter import  *
+from tkinter import ttk
 from tkinter import  messagebox  as mb
-import requestd
-from PIL import Image, ImegaTk
+import requests
+from PIL import Image, ImageTk
 from io import BytesIO
 
 
 
 def get_dog_image():
     try:
-        response = requestd.get("https://dog.ceo/api/breeds/image/random")
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
         response.raise_for_status()
         data = response.json()
-        return data('message')
+        return data['message']
     except Exception as e:
         mb.showerror("Ошибка", f"Возникла ошибка при запросе к API{e}")
         return None
@@ -21,25 +22,34 @@ def show_image():
     image_url = get_dog_image()
     if image_url:
         try:
-            response = requestd.get(image_url, stream=True)
+            response = requests.get(image_url, stream=True)
             response.raise_for_status()
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
-            img.thumbnail(300, 300)
-            img = ImegaTk.PhotoImage(img)
+            img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
             mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображени {e}")
+    progress.stop()
+
+def prog():
+    progress['value'] = 0
+    progress.start(30)
+    window.after(3000, show_image)
 
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
 
-label = Label()
+label = ttk.Label()
 label.pack(pady=10)
 
-button = Button(text="Загрузить изображение", command=show_image)
+button = ttk.Button(text="Загрузить изображение", command=prog)
 button.pack(pady=10)
+
+progress = ttk.Progressbar(mode="determinate", length=300)
+progress.pack(pady=10)
 
 window.mainloop()
